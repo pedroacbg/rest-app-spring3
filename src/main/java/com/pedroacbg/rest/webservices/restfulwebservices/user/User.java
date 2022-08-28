8f72f79c481055f66dab6780bbb6cbc8a0b1410d
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2022-08-28 13:44:45 -0300

    Creating User Entity and some test data

--- src/main/java/com/pedroacbg/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/pedroacbg/rest/webservices/restfulwebservices/user/User.java
@@ -1,13 +1,17 @@
 package com.pedroacbg.rest.webservices.restfulwebservices.user;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+@Entity(name = "user_details")
 public class User {
 
+    @Id
+    @GeneratedValue
     private Long id;
 
     @Size(min = 2, message = "Name should have at least 2 characters")
